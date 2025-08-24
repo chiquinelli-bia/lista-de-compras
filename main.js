f3d9ce14a1206d391c92fbb5835b52154b5b1491
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2025-08-24 15:54:28 -0300

    feat(lista-de-itens): implementar função renderizar os itens da lista

--- main.js
+++ main.js
@@ -1,21 +1,37 @@
-let listaDeCompras;
+let listaDeCompras = [];
 
 const form = document.getElementById("form-itens");
 const inputForm = document.getElementById("receber-item");
+const ulLista = document.getElementById("lista-de-itens");
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   salvarItem();
+  mostrarItens();
 });
 
 function salvarItem() {
   const itemCompras = inputForm.value;
   const checarDuplicado = listaDeCompras.some(
-    (item) => elemento.valor.toUpperCase() === itemCompras.toUpperCase()
+    (item) => item.valor.toUpperCase() === itemCompras.toUpperCase()
   );
   if (checarDuplicado) {
     alert("item já existe na lista de compra");
   } else {
     listaDeCompras.push({ valor: itemCompras });
   }
+}
+function mostrarItens() {
+  ulLista.innerHTML = "";
+  listaDeCompras.forEach((item, index) => {
+    ulLista.innerHTML += `<li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+        <div>
+            <input type="checkbox" class="is-clickable" />
+            <input type="text" class="is-size-5" value="${item.valor}"></input>
+        </div>
+        <div>
+            <i class="fa-solid fa-trash is-clickable deletar"></i>
+        </div>
+    </li>`;
+  });
 }
