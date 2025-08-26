7d985965b2b5c27931d458b5a44823066d28ad5d
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2025-08-26 14:08:13 -0300

    feat(lista-comprados): mover item para a lista de comprados quando marcado e vice-versa

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let listaDeCompras = [];
 const form = document.getElementById("form-itens");
 const inputForm = document.getElementById("receber-item");
 const ulLista = document.getElementById("lista-de-itens");
+const ulListaComprados = document.getElementById("itens-comprados");
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
@@ -23,16 +24,29 @@ function salvarItem() {
 }
 function mostrarItens() {
   ulLista.innerHTML = "";
+  ulListaComprados.innerHTML = "";
   listaDeCompras.forEach((item, index) => {
-    ulLista.innerHTML += `<li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+    if (item.check) {
+      ulListaComprados.innerHTML += `<li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
         <div>
-            <input type="checkbox" class="is-clickable" />
-            <input type="text" class="is-size-5" value="${item.valor}"></input>
+            <input type="checkbox" checked class="is-clickable" />
+            <span class="itens-comprados is-size-5">${item.valor}</span>
         </div>
         <div>
             <i class="fa-solid fa-trash is-clickable deletar"></i>
         </div>
     </li>`;
+    } else {
+      ulLista.innerHTML += `<li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+          <div>
+              <input type="checkbox" class="is-clickable" />
+              <input type="text" class="is-size-5" value="${item.valor}"></input>
+          </div>
+          <div>
+              <i class="fa-solid fa-trash is-clickable deletar"></i>
+          </div>
+      </li>`;
+    }
   });
   const itensCheck = document.querySelectorAll('input[type="checkbox"]');
   itensCheck.forEach((i) => {
@@ -41,6 +55,7 @@ function mostrarItens() {
         evento.target.parentElement.parentElement.getAttribute("data-value");
       listaDeCompras[indiceDoElemento].check = evento.target.checked;
       console.log(listaDeCompras[indiceDoElemento].check);
+      mostrarItens();
     });
   });
 }
